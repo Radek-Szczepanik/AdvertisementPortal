5e52f4388478f0d808e6dbea6bad09e2d10ec515
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2022-01-06 09:57:44 +0100

    Add data to database

---
+++ AdvertisementPortal/AdvertisementPortal/AdvertisementSeeder.cs
@@ -0,0 +1,54 @@
+﻿using AdvertisementPortal.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdvertisementPortal
+{
+    public class AdvertisementSeeder
+    {
+        private readonly AdvertisementDbContext dbContext;
+
+        public AdvertisementSeeder(AdvertisementDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Seed()
+        {
+            if (dbContext.Database.CanConnect())    // czy jest połączenie z bazą danych ?
+            {
+                if (!dbContext.Advertisements.Any())    // czy tabela Advertisement jest pusta ?
+                {
+                    var advertisements = GetAdvertisements();
+                    dbContext.Advertisements.AddRange(advertisements);
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Advertisement> GetAdvertisements()
+        {
+            var advertisements = new List<Advertisement>()
+            {
+                new Advertisement()
+                {
+                    Title = "Hydraulik",
+                    Content = "usługi hydrauliczne",
+                    Email = "hydraulik@test.pl",
+                    PhoneNumber = "111-222-333"
+                },
+
+                new Advertisement()
+                {
+                    Title = "Usługi remontowe",
+                    Content = "malowanie, gipsowanie," +
+                              "układanie płytek",
+                    Email = "uslugi_bud@test.pl",
+                    PhoneNumber = "444-555-666"
+                }
+            };
+
+            return advertisements;
+        }
+    }
+}
--- AdvertisementPortal/AdvertisementPortal/Startup.cs
+++ AdvertisementPortal/AdvertisementPortal/Startup.cs
@@ -1,3 +1,4 @@
+using AdvertisementPortal.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,8 +20,9 @@ namespace AdvertisementPortal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddDbContext<AdvertisementDbContext>();
+            services.AddScoped<AdvertisementSeeder>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdvertisementPortal", Version = "v1" });
@@ -28,8 +30,10 @@ namespace AdvertisementPortal
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AdvertisementSeeder seeder)
         {
+            seeder.Seed();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
